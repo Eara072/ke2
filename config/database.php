15cefa76fadb494c630a0891b9cf7583113a9a7b
Eara032 <azkarama032@gmail.com>
2026-03-02 14:31:44 +0700

    Milii

---
+++ config/database.php
@@ -0,0 +1,16 @@
+'mysql' => [
+    'driver'    => 'mysql',
+    'host'      => env('DB_HOST', '127.0.0.1'),
+    'port'      => env('DB_PORT', 3306),
+    'database'  => env('DB_DATABASE', 'forge'),
+    'username'  => env('DB_USERNAME', 'forge'),
+    'password'  => env('DB_PASSWORD', ''),
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+    'strict'    => false,
+    'engine'    => null,
+    'options'   => [
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    ],
+],
